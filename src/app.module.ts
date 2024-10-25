a10c328a6fa2bf41718adb335f1ac3a4a1f2ed12
Simeao Almeida <simer27@yahoo.com>
2024-10-25 17:34:37 -0300

    criada as controllers, modules, services e as pastas entities e dtos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { TrainingSessionsController } from './training_sessions/training_sessions.controller';
+import { TrainingSessionsService } from './training_sessions/training_sessions.service';
+import { TrainingSessionsModule } from './training_sessions/training_sessions.module';
+import { CheckinController } from './checkin/checkin.controller';
+import { CheckinService } from './checkin/checkin.service';
+import { CheckinModule } from './checkin/checkin.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [UsersModule, TrainingSessionsModule, CheckinModule, AuthModule],
+  controllers: [UsersController, TrainingSessionsController, CheckinController, AuthController],
+  providers: [UsersService, TrainingSessionsService, CheckinService, AuthService],
 })
 export class AppModule {}
---
+++ src/training_sessions/training_sessions.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('training-sessions')
+export class TrainingSessionsController {}
